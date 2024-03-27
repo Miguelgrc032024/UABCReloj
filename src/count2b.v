0b908039e28a55e0086b06b5ac5f5f9464cd6b2b
Miguelgrc032024 <165112479+Miguelgrc032024@users.noreply.github.com>
2024-03-27 00:07:28 -0700

    Add files via upload

---
+++ src/count2b.v
@@ -0,0 +1,25 @@
+
+
+module count2b(
+input ck,
+input rst,
+output [1:0] C0
+);
+
+reg [1:0]Q;
+wire [1:0]D;
+
+always @(negedge ck or posedge rst)
+begin
+  if (rst) begin
+	Q<=2'b00;
+	end
+  else begin
+	Q<=D;
+  end
+end
+
+assign D=Q+1;
+assign C0 = Q;
+
+endmodule
---
+++ src/decod.v
@@ -0,0 +1,17 @@
+
+
+module decod(
+input [1:0] A,
+output C0,
+output C1,
+output C2,
+output C3
+);
+
+
+and U0(C0,~A[1],~A[0]);
+and U1(C1,~A[1],A[0]);
+and U2(C2,A[1],~A[0]);
+and U3(C3,A[1],A[0]);
+
+endmodule
