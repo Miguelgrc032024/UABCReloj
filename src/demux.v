b1ebfefdf6afd242cb0a34a34faf502483300a6f
Miguelgrc032024 <165112479+Miguelgrc032024@users.noreply.github.com>
2024-03-27 13:33:57 -0700

    Update demux.v

--- src/demux.v
+++ src/demux.v
@@ -9,11 +9,15 @@ output  PM1,
 output  PH
 );
 
-and U0(PMx,~selA[1],~selA[0],P0);
-and U1(PM0,~selA[1], selA[0],P0);
-and U2(PM1, selA[1],~selA[0],P0);
-and U3(PH,  selA[1], selA[0],P0);
+//and U0(PMx,~selA[1],~selA[0],P0);
+//and U1(PM0,~selA[1], selA[0],P0);
+//and U2(PM1, selA[1],~selA[0],P0);
+//and U3(PH,  selA[1], selA[0],P0);
 
+assign PMx = (~selA[1]&~selA[0]&P0);
+assign PM0 = (~selA[1]& selA[0]&P0);
+assign PM1 = ( selA[1]&~selA[0]&P0);
+assign PH  = ( selA[1]& selA[0]&P0);
 
 endmodule
 
