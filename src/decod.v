14b0e7958ba826e56d41c4460937a3801e6fe76b
Miguelgrc032024 <165112479+Miguelgrc032024@users.noreply.github.com>
2024-03-27 13:37:38 -0700

    Update decod.v

--- src/decod.v
+++ src/decod.v
@@ -9,11 +9,16 @@ output C3
 );
 
 
-and U0(C0,~A[1],~A[0]);
-and U1(C1,~A[1],A[0]);
-and U2(C2,A[1],~A[0]);
-and U3(C3,A[1],A[0]);
+//and U0(C0,~A[1],~A[0]);
+//and U1(C1,~A[1],A[0]);
+//and U2(C2,A[1],~A[0]);
+//and U3(C3,A[1],A[0]);
 
+assign C0=(~A[1]&~A[0]);
+assign C1=(~A[1]& A[0]);
+assign C2=( A[1]&~A[0]);
+assign C3=( A[1]& A[0]);
+  
 endmodule
 
 
